b26ca2e8cf3ed1e8b3dc0d34afe26b0250d30658
sbx_user1051 <sbx_user1051@169.254.73.221>
2020-03-12 11:48:09 +0000

    Thu Mar 12 11:48:09 UTC 2020

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,20 @@
+package befaster.solutions.HLO;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloSolutionTest {
+
+    private HelloSolution hello;
+
+    @Before
+    public void setup(){
+        hello = new HelloSolution();
+    }
+
+    @Test
+    public void helloTest() {
+    }
+}
